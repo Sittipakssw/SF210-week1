648125f8d23cccbc64e6642c57c0171bf0f87617
SITTIPAK  SRISAWAS <98724180+Sittipakssw@users.noreply.github.com>
2022-02-09 15:07:47 +0000

    Add s

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 while True:
-    age = input("Type your age: ")
+    age = input("Type your ages: ")
     if 0 <= age:
         age = age
         break
     else:
-        age = input("Type your age: ")
+        age = input("Type your ages: ")
 
 while True:
   gpa = input("Type your GPA: ")
   if 0 <= gpa:
     gpa = gpa.round(1)
   else:
-    gpa = input("Type your GPA: ")
+    gpa = input("Type your GPA: ") 
